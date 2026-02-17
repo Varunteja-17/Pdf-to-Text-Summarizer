15a31bb724fcbc7a4c5fe667aaf0d1eb611f0752
Varunteja-17 <varuntejkuncham@gmail.com>
2026-02-17 20:18:28 +0530

    Connected frontend to live Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const uploadPDF = async (formData) => {
   const response = await axios.post(
-    "http://localhost:5000/api/pdf/summarize",
+    "https://pdf-to-text-summarizer-1.onrender.com/api/pdf/summarize",
     formData
   );
   return response.data;
